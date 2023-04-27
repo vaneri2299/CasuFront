975d9686bcabfb6f111f589d68e79635bd2aacd7
Ariana Medina <medina2.ariana9.com>
2023-04-26 20:51:52 -0400

    vite 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // o el valor de puerto que desees
+  },
 })
